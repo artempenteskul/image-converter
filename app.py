2ab7b45d01bd3df1f103f3ce872654429c62e3b9
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-07-28 12:34:25 +0300

    added enum logic

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 from flask import Flask, request, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
 
-from utils import generate_id_for_file, get_filename_from_file_id
+from utils import QualityEnum, generate_id_for_file, get_filename_from_file_id
 
 
 UPLOAD_FOLDER = 'media'
-UPLOAD_FOLDER_ORIGINAL = 'media/100'
+
 
 app = Flask(__name__)
 
@@ -27,23 +27,23 @@ def healthcheck():
 def upload_file():
     if 'file' not in request.files:
         resp = jsonify({'message': 'No file part in the request'})
-        resp.status_code = 404
+        resp.status_code = 400
         return resp
 
     file_id = generate_id_for_file()
 
-    os.makedirs(os.path.join(UPLOAD_FOLDER_ORIGINAL, file_id))
+    os.makedirs(os.path.join(UPLOAD_FOLDER, QualityEnum.HUNDRED.value, file_id))
 
     file = request.files['file']
-    file.save(os.path.join(UPLOAD_FOLDER_ORIGINAL, file_id, secure_filename(file.filename)))
+    file.save(os.path.join(UPLOAD_FOLDER, QualityEnum.HUNDRED.value, file_id, secure_filename(file.filename)))
 
     return jsonify({'message': 'success', 'file_id': file_id})
 
 
 @app.route('/download/<path:file_id>/', methods=['GET'])
 def download_file(file_id):
-    quality = request.args.get('quality', '100')
-    if quality not in ('100', '75', '50', '25'):
+    quality = request.args.get('quality', QualityEnum.HUNDRED.value)
+    if quality not in (q for q in QualityEnum):
         resp = jsonify({'message': 'Incorrect quality value. Supported values: 100, 75, 50 and 25.'})
         resp.status_code = 400
         return resp
--- utils.py
+++ utils.py
@@ -1,17 +1,28 @@
 import os
 import uuid
 
+from enum import Enum
 
-UPLOAD_FOLDER_ORIGINAL = 'media/100'
+UPLOAD_FOLDER = 'media'
+
+
+# check int enum in order to make enum with keys 100, 75, 50, 25
+class QualityEnum(Enum):
+    HUNDRED = '100'
+    SEVENTY_FIVE = '75'
+    FIFTY = '50'
+    TWENTY_FIVE = '25'
 
 
 def generate_id_for_file() -> str:
+    # here we need to add check whether we already use this uuid or not
+    # we can implement it using listdir all folders in original folder and compare existing ids with our
     return str(uuid.uuid4())
 
 
 def get_filename_from_file_id(file_id: str) -> str:
-    if os.path.exists(os.path.join(UPLOAD_FOLDER_ORIGINAL, file_id)):
-        files_in_folder = os.listdir(os.path.join(UPLOAD_FOLDER_ORIGINAL, file_id))
+    if os.path.exists(os.path.join(UPLOAD_FOLDER, '100', file_id)):
+        files_in_folder = os.listdir(os.path.join(UPLOAD_FOLDER, '100', file_id))
         if len(files_in_folder) > 1:
             # probably we need to add better explanation for exception here
             raise Exception('Multiple files for one id.')
