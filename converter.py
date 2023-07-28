034fa494b8149ccbb2659446f2b4cebd1a6ab5e0
Artem Penteskul <artemiy.penteskul@gmail.com>
2023-07-28 13:44:14 +0300

    fixes

--- converter.py
+++ converter.py
@@ -15,7 +15,7 @@ def make_different_qualities_img(file_id: str, filename: str) -> None:
     for img_quality in (QualityEnum.SEVENTY_FIVE.value, QualityEnum.FIFTY.value, QualityEnum.TWENTY_FIVE.value):
         os.makedirs(os.path.join(UPLOAD_FOLDER, img_quality, file_id))
         with Image.open(os.path.join(UPLOAD_FOLDER, QualityEnum.HUNDRED.value, file_id, filename)) as img:
-            img.save(os.path.join(UPLOAD_FOLDER, img_quality, filename), quality=int(img_quality))
+            img.save(os.path.join(UPLOAD_FOLDER, img_quality, file_id, filename), quality=int(img_quality))
 
 
 def process_message(ch, method, properties, body):
